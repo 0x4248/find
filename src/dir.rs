91c995f652cf308d577ce71b359cec2fc57d96c4
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-04-11 11:57:21 +0100

    Username update
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

--- src/dir.rs
+++ src/dir.rs
@@ -1,8 +1,8 @@
 /* Find (dir.rs)
  * A file finder made in rust
- * Github: https://www.github.com/lewisevans2007/find
+ * Github: https://www.github.com/0x4248/find
  * Licence: GNU General Public License v3.0
- * By: Lewis Evans
+ * By: 0x4248
 */
 
 use std::fs;
